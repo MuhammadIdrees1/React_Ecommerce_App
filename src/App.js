2f5f638fd8a6d4f03143c4068a8cc53168adb178
Muhammad Idrees <idreesrehman30@gmail.com>
2023-08-17 01:08:58 +0500

    add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+// sx
 import { useEffect, useState } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import { fetchData } from "./utils/productApi";
 import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
 import Hero from "./components/Hero";
+import HomePage from "./pages/HomePage";
+import Checkout from "./pages/Checkout";
+import ListingPage from "./pages/ListingPage";
+import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -11,8 +19,17 @@ function App() {
 
   return (
     <>
-      {/* <Header /> */}
-      <Hero />
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/listingpage" element={<ListingPage />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+      {/* <Hero /> */}
     </>
   );
 }
